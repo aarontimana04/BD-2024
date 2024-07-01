775e51e6be14acb7c1e784515c7be7282e46082b
aarontimana04 <112721918+aarontimana04@users.noreply.github.com>
2024-06-30 23:45:26 -0500

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,86 +1,100 @@
 CREATE TABLE Sedes (
 	ID SERIAL PRIMARY KEY,
-	nombre VARCHAR(20),
-	correo VARCHAR(20)
+	nombre VARCHAR(20) NOT NULL,
+	correo VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE Clientes (
-	DNI INT PRIMARY KEY,
-	nombre VARCHAR(10),
-	apellido VARCHAR(15)
+	DNI INT PRIMARY KEY NOT NULL,
+	nombre VARCHAR(10) NOT NULL,
+	apellido VARCHAR(15) NOT NULL
 );
 
 CREATE TABLE Conductores (
-	DNI INT PRIMARY KEY,
-	correo VARCHAR(20),
-	nombre VARCHAR(10),
-	apellido VARCHAR(25),
-	sede_id INT,
+	DNI INT PRIMARY KEY NOT NULL,
+	correo VARCHAR(20) NOT NULL,
+	nombre VARCHAR(10) NOT NULL,
+	apellido VARCHAR(25) NOT NULL,
+	sede_id INT NOT NULL,
 	FOREIGN KEY (sede_id) REFERENCES Sedes(ID)
 );
 
 CREATE TABLE Vehiculos (
-	Matricula INT PRIMARY KEY,
-	marca VARCHAR(10),
-	modelo VARCHAR(15),
-	anio_func INT,
-	conductor_dni INT,
+	Matricula VARCHAR(10) PRIMARY KEY NOT NULL,
+	marca VARCHAR(10) NOT NULL,
+	modelo VARCHAR(15) NOT NULL,
+	anio_func INT NOT NULL,
+	conductor_dni INT NOT NULL,
 	FOREIGN KEY (conductor_dni) REFERENCES Conductores(DNI)
 );
 
 CREATE TABLE Estaciones (
-	nombre VARCHAR(15) PRIMARY KEY,
-	ubicacion VARCHAR(10)
+	nombre VARCHAR(15) PRIMARY KEY NOT NULL,
+	ubicacion VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE R_Finalizadas (
 	ID SERIAL PRIMARY KEY,
-	estacion_inicio VARCHAR(15),
-	estacion_final VARCHAR(15),
-	vehiculo_mat INT,
-	hora_s TIME,
-	hora_l TIME,
-	n_embarque_s INT,
-	n_embarque_l INT,
-	fecha DATE,
-	duracion TIME,
-	ingresos_gen INT,
-	distancia INT,
+	estacion_inicio VARCHAR(15) NOT NULL,
+	estacion_final VARCHAR(15) NOT NULL,
+	vehiculo_mat VARCHAR(10) NOT NULL,
+	hora_s TIME NOT NULL,
+	hora_l TIME NOT NULL,
+	n_embarque_s INT NOT NULL,
+	n_embarque_l INT NOT NULL,
+	fecha DATE NOT NULL,
+	duracion TIME NOT NULL,
+	ingresos_gen INT NOT NULL,
+	distancia INT NOT NULL,
 	FOREIGN KEY (estacion_inicio) REFERENCES Estaciones(nombre),
 	FOREIGN KEY (estacion_final) REFERENCES Estaciones(nombre)
 );
 
 CREATE TABLE R_EnCurso (
 	ID SERIAL PRIMARY KEY,
-	fecha DATE,
-	estacion_inicio VARCHAR(15),
-	n_embarque_s INT,
-	hora_s TIME,
-	reporte VARCHAR(100),
+	fecha DATE NOT NULL,
+	estacion_inicio VARCHAR(15) NOT NULL,
+	n_embarque_s INT NOT NULL,
+	hora_s TIME NOT NULL,
+	reporte VARCHAR(100) NOT NULL,
 	FOREIGN KEY (estacion_inicio) REFERENCES Estaciones(nombre)
 );
 
 CREATE TABLE Boletos (
-	id_boleto INT,
-	ruta_id INT,
-	categoria VARCHAR(10),
-	n_asiento INT,
-	costo INT,
+	id_boleto INT NOT NULL,
+	ruta_id INT NOT NULL,
+	categoria VARCHAR(10) NOT NULL,
+	n_asiento INT NOT NULL,
+	costo INT NOT NULL,
 	FOREIGN KEY (ruta_id) REFERENCES R_EnCurso(ID),
 	PRIMARY KEY(id_boleto, ruta_id)
 );
--- mod
+
 CREATE TABLE Compras(
-	boleto_id INT,
-	ruta_id INT,
-	cliente_id INT,
-	metodo_pago VARCHAR(10),
+	boleto_id INT NOT NULL,
+	ruta_id INT NOT NULL,
+	cliente_id INT NOT NULL,
+	metodo_pago VARCHAR(10) NOT NULL,
 	FOREIGN KEY (boleto_id, ruta_id) REFERENCES Boletos(id_boleto, ruta_id),
 	FOREIGN KEY (cliente_id) REFERENCES Clientes(DNI),
 	PRIMARY KEY (boleto_id, ruta_id, cliente_id)
 );
 
---
+-----
+
+CREATE OR REPLACE FUNCTION calcular_duracion () RETURNS TRIGGER AS $$
+BEGIN
+    NEW.duracion := NEW.hora_l - NEW.hora_s;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tg_calcular_duracion
+BEFORE INSERT OR UPDATE ON R_Finalizadas
+FOR EACH ROW
+EXECUTE FUNCTION calcular_duracion();
+
+-----
 
 DROP TABLE clientes;
 DROP TABLE sedes;
@@ -91,4 +105,3 @@ DROP TABLE r_finalizadas;
 DROP TABLE r_encurso;
 DROP TABLE boletos;
 DROP TABLE compras;
-
