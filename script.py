f6c8485aed0c10f557e035c361a9f84a3a673a10
aarontimana04 <112721918+aarontimana04@users.noreply.github.com>
2024-07-02 19:34:11 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,251 +1,238 @@
+from faker import Faker
 import psycopg2
 import random
-from faker import Faker
 
-# Crear una instancia de Faker
-fake = Faker()
+fake = Faker(['es_ES', 'fr_FR', 'it_IT', 'de_DE'])
 
-# Conexión a la base de datos
-conn = psycopg2.connect(
-    dbname='dbmil',
-    user='postgres',
-    password='password',
-    host='localhost',
-    port='5432'
-)
-cursor = conn.cursor()
+def get_connection():
+    db_params = {
+        'host': 'localhost',
+        'database': 'db100mil',
+        'user': 'postgres',
+        'password': 'password',
+        'port': '5432',
+    }
+    connection = psycopg2.connect(**db_params)
+    connection.autocommit = True
+    print("¡Conexión exitosa a la base de datos!")
+    return connection, connection.cursor()
 
+def generate_sedes():
+    nombre = fake.company()[:20]
+    correo = fake.email()[:20]
+    return {
+        'nombre': nombre,
+        'correo': correo
+    }
 
-# Funciones para poblar las tablas
-def populate_sedes(num_records):
-    for _ in range(num_records):
-        nombre = fake.company()[:20]
-        correo = fake.email()[:20]
-        cursor.execute(
-            """
-            INSERT INTO Sedes (nombre, correo)
-            VALUES (%s, %s)
-            """,
-            (nombre, correo)
-        )
+def generate_unique_dni(existing_dnis):
+    while True:
+        dni = ''.join(random.choice('123456789') for _ in range(8))
+        if dni not in existing_dnis:
+            existing_dnis.add(dni)
+            return dni
 
+def generate_clientes(existing_dnis):
+    dni = generate_unique_dni(existing_dnis)
+    nombre = fake.first_name()[:10]
+    apellido = fake.last_name()[:15]
+    return {
+        'DNI': dni,
+        'nombre': nombre,
+        'apellido': apellido
+    }
 
-def populate_clientes(num_records):
-    dnis = set()
-    while len(dnis) < num_records:
-        dnis.add(random.randint(10000000, 99999999))
+def generate_conductores(existing_dnis, sede_id):
+    dni = generate_unique_dni(existing_dnis)
+    correo = fake.email()[:20]
+    nombre = fake.first_name()[:10]
+    apellido = fake.last_name()[:25]
+    return {
+        'DNI': dni,
+        'correo': correo,
+        'nombre': nombre,
+        'apellido': apellido,
+        'sede_id': sede_id
+    }
 
-    for dni in dnis:
-        nombre = fake.first_name()[:10]
-        apellido = fake.last_name()[:15]
-        cursor.execute(
-            """
-            INSERT INTO Clientes (DNI, nombre, apellido)
-            VALUES (%s, %s, %s)
-            """,
-            (dni, nombre, apellido)
-        )
+def generate_unique_matricula(existing_matriculas):
+    while True:
+        matricula = fake.license_plate()[:10]
+        if matricula not in existing_matriculas:
+            existing_matriculas.add(matricula)
+            return matricula
 
+def generate_vehiculos(existing_matriculas, dni):
+    matricula = generate_unique_matricula(existing_matriculas)
+    marca = fake.company()[:10]
+    modelo = fake.word()[:15]
+    anio_func = fake.year()
+    return {
+        'Matricula': matricula,
+        'marca': marca,
+        'modelo': modelo,
+        'anio_func': anio_func,
+        'conductor_dni': dni
+    }
 
-def populate_conductores(num_records):
-    cursor.execute("SELECT ID FROM Sedes")
-    sedes_ids = [row[0] for row in cursor.fetchall()]
+def generate_estaciones():
+    nombre = fake.city()[:15]
+    ubicacion = fake.address()[:10]
+    return {
+        'nombre': nombre,
+        'ubicacion': ubicacion
+    }
 
-    dnis = set()
-    while len(dnis) < num_records:
-        dnis.add(random.randint(10000000, 99999999))
+def generate_r_finalizadas(estacion_inicio, estacion_final):
+    vehiculo_mat = fake.license_plate()[:10]
+    hora_s = fake.time()
+    hora_l = fake.time()
+    n_embarque_s = random.randint(1, 100)
+    n_embarque_l = random.randint(1, 100)
+    fecha = fake.date_this_year()
+    duracion = None
+    ingresos_gen = random.randint(100, 10000)
+    distancia = random.randint(1, 1000)
+    return {
+        'estacion_inicio': estacion_inicio,
+        'estacion_final': estacion_final,
+        'vehiculo_mat': vehiculo_mat,
+        'hora_s': hora_s,
+        'hora_l': hora_l,
+        'n_embarque_s': n_embarque_s,
+        'n_embarque_l': n_embarque_l,
+        'fecha': fecha,
+        'duracion': duracion,
+        'ingresos_gen': ingresos_gen,
+        'distancia': distancia
+    }
 
-    for dni in dnis:
-        correo = fake.email()[:20]
-        nombre = fake.first_name()[:10]
-        apellido = fake.last_name()[:25]
-        sede_id = random.choice(sedes_ids)
-        cursor.execute(
-            """
-            INSERT INTO Conductores (DNI, correo, nombre, apellido, sede_id)
-            VALUES (%s, %s, %s, %s, %s)
-            """,
-            (dni, correo, nombre, apellido, sede_id)
-        )
+def generate_r_encurso(estacion_inicio):
+    fecha = fake.date_this_year()
+    n_embarque_s = random.randint(1, 100)
+    hora_s = fake.time()
+    reporte = fake.text(max_nb_chars=100)
+    return {
+        'fecha': fecha,
+        'estacion_inicio': estacion_inicio,
+        'n_embarque_s': n_embarque_s,
+        'hora_s': hora_s,
+        'reporte': reporte
+    }
 
+def generate_unique_boleto_id(existing_boletos):
+    while True:
+        id_boleto = random.randint(1, 1000000)
+        if id_boleto not in existing_boletos:
+            existing_boletos.add(id_boleto)
+            return id_boleto
 
-def populate_vehiculos(num_records):
-    cursor.execute("SELECT DNI FROM Conductores")
-    conductores_dnis = [row[0] for row in cursor.fetchall()]
+def generate_boletos(existing_boletos, ruta_id):
+    id_boleto = generate_unique_boleto_id(existing_boletos)
+    categoria = random.choice(['vip', 'estandar', 'premium'])[:10]
+    n_asiento = random.randint(1, 100)
+    costo = random.randint(10, 1000)
+    return {
+        'id_boleto': id_boleto,
+        'ruta_id': ruta_id,
+        'categoria': categoria,
+        'n_asiento': n_asiento,
+        'costo': costo
+    }
 
-    matriculas = set()
-    while len(matriculas) < num_records:
-        matriculas.add(random.randint(1000000, 9999999))
+def generate_compras(boleto_id, ruta_id, cliente_id):
+    metodo_pago = random.choice(['Efectivo', 'Tarjeta', 'Transferencia'])[:10]
+    return {
+        'boleto_id': boleto_id,
+        'ruta_id': ruta_id,
+        'cliente_id': cliente_id,
+        'metodo_pago': metodo_pago
+    }
 
-    for matricula in matriculas:
-        marca = fake.company()[:10]
-        modelo = fake.word()[:15]
-        anio_func = random.randint(1990, 2023)
-        conductor_dni = random.choice(conductores_dnis)
-        cursor.execute(
-            """
-            INSERT INTO Vehiculos (Matricula, marca, modelo, anio_func, conductor_dni)
-            VALUES (%s, %s, %s, %s, %s)
-            """,
-            (matricula, marca, modelo, anio_func, conductor_dni)
-        )
+def insert_data(cursor, table, data):
+    columns = ', '.join(data.keys())
+    values = ', '.join(['%s'] * len(data))
+    query = f"INSERT INTO {table} ({columns}) VALUES ({values})"
+    cursor.execute(query, tuple(data.values()))
 
+def populate_database():
+    try:
+        connection, cursor = get_connection()
 
-def populate_estaciones(num_records):
-    unique_names = set()
-    while len(unique_names) < num_records:
-        unique_names.add(fake.city()[:15])
+        unique_dnis = set()
+        unique_matriculas = set()
+        unique_boletos = set()
 
-    for nombre in unique_names:
-        ubicacion = fake.state_abbr()[:10]
-        cursor.execute(
-            """
-            INSERT INTO Estaciones (nombre, ubicacion)
-            VALUES (%s, %s)
-            """,
-            (nombre, ubicacion)
-        )
+        # Poblar la tabla Sedes con 10 tuplas
+        for _ in range(10):
+            sedes = generate_sedes()
+            insert_data(cursor, 'Sedes', sedes)
+            cursor.execute("SELECT currval('sedes_id_seq');")
+            sede_id = cursor.fetchone()[0]
 
+            # Poblar la tabla Conductores con 10,000 tuplas
+            for _ in range(10000):
+                try:
+                    conductores = generate_conductores(unique_dnis, sede_id)
+                    insert_data(cursor, 'Conductores', conductores)
 
-def populate_r_encurso(num_records):
-    cursor.execute("SELECT nombre FROM Estaciones")
-    estaciones_nombres = [row[0] for row in cursor.fetchall()]
+                    # Poblar la tabla Vehiculos con 10,000 tuplas
+                    vehiculos = generate_vehiculos(unique_matriculas, conductores['DNI'])
+                    insert_data(cursor, 'Vehiculos', vehiculos)
+                except psycopg2.IntegrityError as e:
+                    print(f"IntegrityError: {e}")
+                    connection.rollback()
 
-    for _ in range(num_records):
-        estacion_inicio = random.choice(estaciones_nombres)
-        n_embarque_s = random.randint(1, 100)
-        hora_s = fake.time()
-        reporte = fake.text(max_nb_chars=100)
-        cursor.execute(
-            """
-            INSERT INTO R_EnCurso (fecha, estacion_inicio, n_embarque_s, hora_s, reporte)
-            VALUES (CURRENT_DATE, %s, %s, %s, %s)
-            """,
-            (estacion_inicio, n_embarque_s, hora_s, reporte)
-        )
+        # Poblar la tabla Estaciones con 10 tuplas
+        for _ in range(10):
+            estaciones = generate_estaciones()
+            insert_data(cursor, 'Estaciones', estaciones)
+            estacion_inicio = estaciones['nombre']
 
+            # Poblar la tabla R_EnCurso con 10,000 tuplas
+            for _ in range(10000):
+                try:
+                    r_encurso = generate_r_encurso(estacion_inicio)
+                    insert_data(cursor, 'R_EnCurso', r_encurso)
+                    cursor.execute("SELECT currval('r_encurso_id_seq');")
+                    ruta_id = cursor.fetchone()[0]
 
-def populate_boletos(num_records):
-    cursor.execute("SELECT ID FROM R_EnCurso")
-    rutas_ids = [row[0] for row in cursor.fetchall()]
-    categorias = ['estandar', 'vip', 'premium']
+                    # Poblar la tabla Boletos con 1,000,000 tuplas
+                    for _ in range(1):
+                        try:
+                            boletos = generate_boletos(unique_boletos, ruta_id)
+                            insert_data(cursor, 'Boletos', boletos)
 
-    boletos_ids = set()
-    while len(boletos_ids) < num_records:
-        boletos_ids.add(random.randint(100000, 999999))
+                            # Poblar la tabla Compras con 1,000,000 tuplas
+                            clientes = generate_clientes(unique_dnis)
+                            insert_data(cursor, 'Clientes', clientes)
+                            compras = generate_compras(boletos['id_boleto'], ruta_id, clientes['DNI'])
+                            insert_data(cursor, 'Compras', compras)
+                        except psycopg2.IntegrityError as e:
+                            print(f"IntegrityError: {e}")
+                            connection.rollback()
+                except psycopg2.IntegrityError as e:
+                    print(f"IntegrityError: {e}")
+                    connection.rollback()
 
-    for id_boleto in boletos_ids:
-        ruta_id = random.choice(rutas_ids)
-        categoria = random.choice(categorias)
-        n_asiento = random.randint(1, 50)
-        costo = random.randint(10, 100)
-        cursor.execute(
-            """
-            INSERT INTO Boletos (id_boleto, ruta_id, categoria, n_asiento, costo)
-            VALUES (%s, %s, %s, %s, %s)
-            """,
-            (id_boleto, ruta_id, categoria, n_asiento, costo)
-        )
+        # Poblar la tabla R_Finalizadas con 10,000 tuplas
+        for _ in range(100000):
+            try:
+                estacion_inicio = fake.city()[:15]
+                estacion_final = fake.city()[:15]
+                r_finalizadas = generate_r_finalizadas(estacion_inicio, estacion_final)
+                insert_data(cursor, 'R_Finalizadas', r_finalizadas)
+            except psycopg2.IntegrityError as e:
+                print(f"IntegrityError: {e}")
+                connection.rollback()
 
+        print("Datos insertados con éxito.")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        if connection:
+            connection.close()
+            print("Conexión cerrada.")
 
-def populate_compras(num_records):
-    cursor.execute("SELECT id_boleto, ruta_id FROM Boletos")
-    boletos = cursor.fetchall()
-    cursor.execute("SELECT DNI FROM Clientes")
-    clientes_dnis = [row[0] for row in cursor.fetchall()]
-
-    unique_combinations = set()
-    for _ in range(num_records):
-        while True:
-            boleto = random.choice(boletos)
-            cliente_id = random.choice(clientes_dnis)
-            if (boleto[0], boleto[1], cliente_id) not in unique_combinations:
-                unique_combinations.add((boleto[0], boleto[1], cliente_id))
-                break
-        metodo_pago = fake.credit_card_provider()[:10]
-        cursor.execute(
-            """
-            INSERT INTO Compras (boleto_id, ruta_id, cliente_id, metodo_pago)
-            VALUES (%s, %s, %s, %s)
-            """,
-            (boleto[0], boleto[1], cliente_id, metodo_pago)
-        )
-
-
-def populate_r_finalizadas(num_records):
-    cursor.execute("SELECT nombre FROM Estaciones")
-    estaciones_nombres = [row[0] for row in cursor.fetchall()]
-    cursor.execute("SELECT Matricula FROM Vehiculos")
-    vehiculos_mats = [row[0] for row in cursor.fetchall()]
-
-    for _ in range(num_records):
-        estacion_inicio = random.choice(estaciones_nombres)
-        estacion_final = random.choice(estaciones_nombres)
-        vehiculo_mat = random.choice(vehiculos_mats)
-        hora_s = fake.time()
-        hora_l = fake.time()
-        n_embarque_s = random.randint(1, 100)
-        n_embarque_l = random.randint(1, 100)
-        fecha = fake.date_this_year()
-        duracion = None
-        ingresos_gen = random.randint(1000, 10000)
-        distancia = random.randint(10, 200)
-        cursor.execute(
-            """
-            INSERT INTO R_Finalizadas (estacion_inicio, estacion_final, vehiculo_mat, hora_s, hora_l, n_embarque_s, n_embarque_l, fecha, duracion, ingresos_gen, distancia)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            """,
-            (estacion_inicio, estacion_final, vehiculo_mat, hora_s, hora_l, n_embarque_s, n_embarque_l, fecha, duracion,
-             ingresos_gen, distancia)
-        )
-
-
-# Llamar a las funciones para poblar las tablas
-# 1M
-populate_sedes(10)
-populate_clientes(1000000)
-populate_conductores(10000)
-populate_vehiculos(10000)
-populate_estaciones(10)
-populate_r_encurso(10000)
-populate_boletos(1000000)
-populate_compras(1000000)
-populate_r_finalizadas(10000)
-#100K
-populate_sedes(10)
-populate_clientes(100000)
-populate_conductores(1000)
-populate_vehiculos(1000)
-populate_estaciones(10)
-populate_r_encurso(1000)
-populate_boletos(100000)
-populate_compras(100000)
-populate_r_finalizadas(1000)
-#10K
-populate_sedes(10)
-populate_clientes(10000)
-populate_conductores(100)
-populate_vehiculos(100)
-populate_estaciones(10)
-populate_r_encurso(100)
-populate_boletos(10000)
-populate_compras(10000)
-populate_r_finalizadas(100)
-#1K
-populate_sedes(10)
-populate_clientes(1000)
-populate_conductores(100)
-populate_vehiculos(100)
-populate_estaciones(10)
-populate_r_encurso(100)
-populate_boletos(1000)
-populate_compras(1000)
-populate_r_finalizadas(100)
-
-# Confirmar los cambios en la base de datos
-conn.commit()
-
-# Cerrar la conexión
-cursor.close()
-conn.close()
-
+if __name__ == "__main__":
+    populate_database()
